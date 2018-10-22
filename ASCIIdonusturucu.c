67a115ef699fef1b8ab5a481074eb8d724479675
Toygar <44132720+Toygarr@users.noreply.github.com>
2018-10-22 21:15:48 +0300

    C dilinde ASCII harf-sayi donusumu yapan kod.

---
+++ ASCIIdonusturucu.c
@@ -0,0 +1,13 @@
+ #include<stdio.h>
+
+ int main()
+ {
+
+    char karakter;
+    printf("ASCII tablosundaki degerini ogrenmek istediginiz harfi yaziniz =>");
+    scanf("%c",&karakter);
+    
+    printf("%c karakterinin ASCII tablosundaki degeri %d sayisidir\n",karakter,karakter);
+
+return 0;
+ }
