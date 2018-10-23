3dbf6828c3b7107eb4610de1fa3b456b42b327b1
Toygar <44132720+Toygarr@users.noreply.github.com>
2018-10-23 20:32:43 +0300

    Fibonacci, 20 ye kadar olan 3 e bolunen elemanlar

--- Fibonacci_ornek.c
+++ Fibonacci_ornek.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 
-int A,B,C,i,n;
+int A,B,C,i;
 
 int main (){
-
-    A=1; B=1; 
-    int n;
-    printf("Kacinci Fibonacci elemanina kadar bakmak istersiniz? : ");
-    scanf("%d'ye kadar olan, 3'e bolunebilen dizi elemanları bunlardir.",&n);
-
     
-    for (i=3; i<n; i++){
-
+    A=1; B=1;
+    
+    printf("%d\n%d",A,B);
+    
+    for (i=3; i<20; i++){
         C=A+B;
         A=B;
         B=C;
         if(C%3==0)
             printf("%d\n",C);
     }
-
+   
     return 0 ;
 }
